b3af567c594f6228f324809cf7f579e97248e960
Sperandío Rodríguez, Diego Alejandro <dasperandio.22@est.ucab.edu.ve>
2025-07-06 17:33:31 -0400

    Add dish entity for menu module
    -Added: dish_entity.py

---
+++ app/menu/domain/entities/dish_entity.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+from uuid import UUID
+from app.auth.domain.entities.role_entity import Role
+
+class Category(str, Enum):
+    STARTER = "Starter"
+    MAIN_COURSE = "Main"
+    DESSERT = "Dessert" 
+    DRINK = "Drink"
+
+class Dish(BaseModel):
+    id: UUID | None =None #para cuando se crea un usuario, no se tiene el id
+    name: str
+    description: str
+    category: Category
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
