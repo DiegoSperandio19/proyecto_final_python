4175de7430d1122b766667e1c88ec7cfc3cb0186
Sperandío Rodríguez, Diego Alejandro <dasperandio.22@est.ucab.edu.ve>
2025-07-07 17:48:15 -0400

    Add ReservationOut DTO and response models to endpoints

--- app/reservation/api/controllers/reservation_router.py
+++ app/reservation/api/controllers/reservation_router.py
@@ -11,7 +11,7 @@ from app.auth.infraestructure.utils.auth_utils import ACCESS_TOKEN_EXPIRE_MINUTE
 from app.db import get_session
 from app.reservation.domain.entities.reservation_entity import Reservation
 from app.reservation.domain.services.reservation_service import ReservationService
-from app.reservation.domain.value_objects.reservation_dto import ReservationCreate
+from app.reservation.domain.value_objects.reservation_dto import ReservationCreate, ReservationOut
 from app.reservation.infrastructure.repositories.orm_reservation_repository import SQLReservationRepository
 from app.restaurants.infrastructure.repositories.orm_restaurant_repository import SQLAlchemyRestaurantRepository
 from app.restaurants.infrastructure.repositories.orm_table_repository import SQLAlchemyTableRepository
@@ -25,7 +25,7 @@ async def get_reservation_service(session: AsyncSession = Depends(get_session)):
     restaurant_repo = SQLAlchemyRestaurantRepository(session)
     return ReservationService(reservation_repo, table_repo, restaurant_repo)
 
-@reservation_router.post("/register")
+@reservation_router.post("/register", response_model=ReservationOut, status_code=status.HTTP_201_CREATED)
 async def create_reservation(
     reservation: ReservationCreate,
     get_reservation_service: Annotated[ReservationService, Depends(get_reservation_service)],
@@ -50,7 +50,7 @@ async def create_reservation(
         )
     return reservation
 
-@reservation_router.put("/cancel/{reservation_id}")
+@reservation_router.put("/cancel/{reservation_id}", response_model=ReservationOut, status_code=status.HTTP_200_OK)
 async def cancel_reservation(
     reservation_id: UUID,
     get_reservation_service: Annotated[ReservationService, Depends(get_reservation_service)],
--- app/reservation/domain/value_objects/reservation_dto.py
+++ app/reservation/domain/value_objects/reservation_dto.py
@@ -2,9 +2,19 @@ from datetime import time
 from pydantic import BaseModel
 from uuid import UUID
 
+from app.reservation.domain.entities.reservation_entity import ReservationStatus
+
 
 
 class ReservationCreate(BaseModel): 
     id_table: UUID 
     start_time: time
     end_time: time
+
+class ReservationOut(BaseModel): 
+    id: UUID
+    id_user: UUID 
+    id_table: UUID 
+    start_time: time
+    end_time: time
+    status: ReservationStatus
