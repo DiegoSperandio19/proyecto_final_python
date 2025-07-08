125e834418ec149b5ad3030b8e1af1bc801fbce5
JuanJoseCammarano <149250549+JuanJoseCammarano@users.noreply.github.com>
2025-07-08 04:34:40 +0000

    Added auth to restaurants module endpoints

--- app/restaurants/api/controllers/restaurant_router.py
+++ app/restaurants/api/controllers/restaurant_router.py
@@ -7,6 +7,9 @@ from app.restaurants.infrastructure.repositories.orm_restaurant_repository impor
 from uuid import UUID
 from app.db import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
+from app.auth.infraestructure.utils.auth_utils import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_current_user, require_admin_role, require_client_role
+from typing import Annotated
+from app.shared.exceptions import DishNotFound, InvalidName, User
 restaurant_router = APIRouter()
 
 async def get_restaurant_service(session: AsyncSession = Depends(get_session)):
@@ -14,21 +17,21 @@ async def get_restaurant_service(session: AsyncSession = Depends(get_session)):
     return RestaurantService(repository)
 
 @restaurant_router.get("/restaurants/", response_model=list[Restaurant])
-async def get_restaurants(restaurant_service: RestaurantService = Depends(get_restaurant_service)):
+async def get_restaurants(get_current_user: Annotated[User, Depends(require_client_role)], restaurant_service: RestaurantService = Depends(get_restaurant_service)):
     restaurants= await restaurant_service.get_all_restaurants()
     return restaurants
 
 @restaurant_router.post("/restaurants/", response_model=Restaurant)
-async def add_restaurant(restaurant: RestaurantCreate, restaurant_service: RestaurantService = Depends(get_restaurant_service)):
+async def add_restaurant(get_current_user: Annotated[User, Depends(require_admin_role)], restaurant: RestaurantCreate, restaurant_service: RestaurantService = Depends(get_restaurant_service)):
     restaurant= await restaurant_service.add_restaurant(restaurant)
     return restaurant
 
 @restaurant_router.put("/restaurants/{restaurant_id}",response_model=Restaurant)
-async def update_restaurant(restaurant_id: UUID, restaurant: RestaurantUpdate, restaurant_service: RestaurantService = Depends(get_restaurant_service)):
+async def update_restaurant(get_current_user: Annotated[User, Depends(require_admin_role)],restaurant_id: UUID, restaurant: RestaurantUpdate, restaurant_service: RestaurantService = Depends(get_restaurant_service)):
     restaurant=await restaurant_service.update_restaurant(restaurant_id, restaurant)
     return restaurant
 
 @restaurant_router.delete("/restaurants/{restaurant_id}",response_model=Restaurant)
-async def delete_restaurant(restaurant_id: UUID,restaurant_service: RestaurantService = Depends(get_restaurant_service)):
+async def delete_restaurant(get_current_user: Annotated[User, Depends(require_admin_role)],restaurant_id: UUID,restaurant_service: RestaurantService = Depends(get_restaurant_service)):
     restaurant= await restaurant_service.soft_delete_restaurant(restaurant_id)
     return restaurant
