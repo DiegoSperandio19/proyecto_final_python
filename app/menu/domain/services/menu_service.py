55e5d4500d0d8ebf4b6d6b29170b43d68eaa992c
Sperandío Rodríguez, Diego Alejandro <dasperandio.22@est.ucab.edu.ve>
2025-07-07 22:52:22 -0400

    Support multiple dishes in preorder creation
    Refactored preorder creation logic to allow preordering multiple dishes at once, enforcing a maximum of 5 dishes per preorder. Updated DTOs to support lists of dishes and adjusted service logic to handle batch creation and output of preorders.

--- app/menu/domain/services/menu_service.py
+++ app/menu/domain/services/menu_service.py
@@ -5,10 +5,10 @@ from app.menu.domain.entities.dish_entity import Dish
 from app.menu.domain.entities.preorder_entity import Preorder
 from app.menu.domain.repositories.dish_repository import DishRepository
 from app.menu.domain.repositories.preorder_repository import PreorderRepository
-from app.menu.domain.value_object.dish_dto import DishCreate, DishUpdate
-from app.menu.domain.value_object.preorder_dto import PreorderCreate
+from app.menu.domain.value_object.dish_dto import DishCreate, DishOut, DishUpdate
+from app.menu.domain.value_object.preorder_dto import ListDishes, PreorderCreate, PreorderListOut, PreorderOut
 from app.reservation.domain.repositories.reservation_repository import ReservationRepository
-from app.shared.exceptions import DishNotFound, InvalidName
+from app.shared.exceptions import DishNotFound, InvalidName, ReservationNotFound
 
 class MenuService:
     def __init__(self, dish_repo: DishRepository, preorder_repo: PreorderRepository, reservation_repo: ReservationRepository):
@@ -56,12 +56,42 @@ class MenuService:
         return await self.dish_repo.update_dish(existing_dish)
     
     async def create_preorder(self, preorder_data: PreorderCreate, user: User) -> None:
-        existig_reservation = await self.reservation_repo.get_reservation_by_id(preorder_data.id_reservation)
-        preorder=Preorder(
+        existing_reservation = await self.reservation_repo.get_reservation_by_id(preorder_data.id_reservation)
+        if not existing_reservation:
+            raise ReservationNotFound(preorder_data.id_reservation)
+        
+        cant_dishes = 0
+        for dish in preorder_data.dishes:
+            cant_dishes += dish.n_dishes
+        if cant_dishes > 5:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="You can only preorder up to 5 dishes."
+            )
+        list_preorders = []
+        for dish in preorder_data.dishes:
+            existing_dish = await self.dish_repo.get_dish_by_id(dish_id=dish.id_dish)
+            if not existing_dish:
+                raise DishNotFound(dish.id_dish)
+            preorder=Preorder(
+                id_reservation=preorder_data.id_reservation,
+                id_user=user.id,
+                id_table=existing_reservation.id_table,
+                id_dish=dish.id_dish,
+                n_dishes=dish.n_dishes
+            )
+            preorder_created = await self.preorder_repo.create_preorder(preorder)
+            preorder_append = PreorderOut(
+                id_preorder=preorder_created.id,
+                n_dishes=preorder_created.n_dishes
+            )
+            list_preorders.append(preorder_append)
+
+        dish_out = PreorderListOut(
             id_reservation=preorder_data.id_reservation,
             id_user=user.id,
-            id_table=existig_reservation.id_table,
-            id_dish=preorder_data.id_dish,
-            n_dishes=preorder_data.n_dishes
+            id_table=existing_reservation.id_table,
+            list_preorders=list_preorders
         )
-        return await self.preorder_repo.create_preorder(preorder)
+        
+        return dish_out
--- app/menu/domain/value_object/preorder_dto.py
+++ app/menu/domain/value_object/preorder_dto.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel
 from uuid import UUID
 
-class PreorderCreate(BaseModel):
-    id_reservation: UUID
+class ListDishes(BaseModel):
     id_dish: UUID
     n_dishes: int
 
+class PreorderCreate(BaseModel):
+    id_reservation: UUID
+    dishes: list[ListDishes]
+
+class PreorderOut(BaseModel):
+    id_preorder: UUID
+    n_dishes: int
+
+class PreorderListOut(BaseModel):
+    id_reservation: UUID
+    id_user: UUID
+    id_table: UUID
+    list_preorders: list[PreorderOut]
